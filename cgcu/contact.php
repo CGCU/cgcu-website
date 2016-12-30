18bf8403f2ee7ea6f9efe74dd28e9509c7460fb6
andrewhill <andrew.hill15@imperial.ac.uk>
2016-12-30 01:32:05 +0000

    PHP Contact Form working

--- cgcu/contact.php
+++ cgcu/contact.php
@@ -1,3 +1,49 @@
+<?php
+    if (isset($_POST["submit"])) {
+        $name = $_POST['name'];
+        $email = $_POST['email'];
+        $message = $_POST['message'];
+        $human = intval($_POST['human']);
+        $from = 'Website Contact Form';
+        $to = 'guilds@imperial.ac.uk';
+        $subject = 'Message from Website Form from: ' . $email;
+
+        $body ="From: $name\n E-Mail: $email\n Message:\n $message";
+
+        // Check if name has been entered
+        $errName = "";
+        if (!$_POST['name']) {
+            $errName = 'Please enter your name';
+        }
+
+        // Check if email has been entered and is valid
+        $errEmail = "";
+        if (!$_POST['email'] || !filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)) {
+            $errEmail = 'Please enter a valid email address';
+        }
+
+        //Check if message has been entered
+        $errMessage = "";
+        if (!$_POST['message']) {
+            $errMessage = 'Please enter your message';
+        }
+        //Check if simple anti-bot test is correct
+        $errHuman = "";
+        if ($human !== 5) {
+            $errHuman = 'Your anti-spam is incorrect';
+        }
+
+        // If there are no errors, send the email
+        if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
+            if (mail ($to, $subject, $body, $from)) {
+                $result='<div class="alert alert-success">Thank You! Your message has been sent to the committee</div>';
+            } else {
+                $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later or email <strong>guilds@imperial.ac.uk</strong> instead.</div>';
+            }
+        }
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="en">
     <head>
@@ -89,7 +135,7 @@
                             <li><a href="cgca.html">CGCA</a></li>
                             <li><a href="regalia.html">Regalia</a></li>
                             <li><a href="sponsors.html">Sponsors</a></li>
-                            <li class="active"><a href="contact.html">Contact Us</a></li>
+                            <li class="active"><a href="contact.php">Contact Us</a></li>
 
                         </ul>
 
@@ -143,6 +189,58 @@
                 and one of our committee will get back to you.
             </p>
         </div>
+
+        <!-- CONTACT FORM -->
+        <!-- https://github.com/bootstrapbay/contact-form/blob/master/index.php -->
+
+        <div class="container well white-bkg">
+            <div class="row">
+                <div class="col-md-6 col-md-offset-3">
+                    <form class="form-horizontal" role="form" method="post" action="contact.php">
+                        <div class="form-group">
+                            <label for="name" class="col-sm-2 control-label">Name</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="name" name="name" placeholder="First & Last Name" value="<?php echo htmlspecialchars($_POST['name']); ?>">
+                                <?php echo "<p class='text-danger'>$errName</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="email" class="col-sm-2 control-label">Email</label>
+                            <div class="col-sm-10">
+                                <input type="email" class="form-control" id="email" name="email" placeholder="example@imperial.ac.uk" value="<?php echo htmlspecialchars($_POST['email']); ?>">
+                                <?php echo "<p class='text-danger'>$errEmail</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="message" class="col-sm-2 control-label">Message</label>
+                            <div class="col-sm-10">
+                                <textarea class="form-control" rows="4" name="message"><?php echo htmlspecialchars($_POST['message']);?></textarea>
+                                <?php echo "<p class='text-danger'>$errMessage</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <label for="human" class="col-sm-2 control-label">2 + 3 = ?</label>
+                            <div class="col-sm-10">
+                                <input type="text" class="form-control" id="human" name="human" placeholder="Your Answer">
+                                <?php echo "<p class='text-danger'>$errHuman</p>";?>
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <input id="submit" name="submit" type="submit" value="Send" class="btn btn-primary">
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-sm-10 col-sm-offset-2">
+                                <?php echo $result; ?>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+
+        <!-- END CONTACT FORM -->
 
         <!-- FOOTER -->
         <footer>
