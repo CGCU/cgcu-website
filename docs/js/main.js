b0359ab6a5577e0061ef599f02e155488cf648f9
Chung Poon <cmpoon212@gmail.com>
2017-08-07 00:27:59 +0100

    Update empty pages with content and update template across all pages

--- docs/js/main.js
+++ docs/js/main.js
@@ -3,11 +3,25 @@
  * Created by andrewhill on 17/07/2016.
  */
 
-/**
-* Contact form modal
-**/
+
 $().ready(function(){
-  if (window.location.search.search("thanks") !== -1){
+
+  /**
+  * Contact form modal
+  **/
+  if (window.location.href.search("contact.html") !== -1 && window.location.search.search("thanks") !== -1){
     $('#thanksModal').modal('show')
+  }
+
+  /**
+  * Comittee table loader
+  **/
+  if (window.location.href.search("committee.html") !== -1){
+    var thisYear = "2017";
+
+    $.get("committeeTabs.php?year="+year, function( data ) {
+      $("#committeeInfo").html( data );
+      console.log( "Committee info loaded." );
+    });
   }
 });
