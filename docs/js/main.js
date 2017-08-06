382074b5ab75c9cde55eedddfa5b75d337bf4861
Chung Poon <cmpoon212@gmail.com>
2017-08-06 18:53:49 +0100

    Created new static Contacts page. Adjusted jumbotron theme for h1.

--- docs/js/main.js
+++ docs/js/main.js
@@ -2,3 +2,12 @@
  * Updated by cmpoon
  * Created by andrewhill on 17/07/2016.
  */
+
+/**
+* Contact form modal
+**/
+$().ready(function(){
+  if (window.location.search.search("thanks") !== -1){
+    $('#thanksModal').modal('show')
+  }
+});
